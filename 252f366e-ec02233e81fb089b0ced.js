3c47054044cd972296ea08e8e0e2bc9ea676d45c
Fedor Rodin <rodinfm307@ya.ru>
2023-10-23 00:46:23 +0300

    first commit

---
+++ 252f366e-ec02233e81fb089b0ced.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_chakra_ui_type_script=self.webpackChunkgatsby_starter_chakra_ui_type_script||[]).push([[228],{5434:function(t,a,r){r.d(a,{hdK:function(){return n}});var e=r(4405);function n(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-ec02233e81fb089b0ced.js.map
